b79f9e02a0bc866aa2ed2028bd3c29125696c28d
Nikola <mtsebrov@gmail.com>
2023-01-31 21:53:17 +0200

    update

--- MagicEvents/Events/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-
-
-# Create your views here.
-
-@csrf_exempt
-def events(request):
-    return render(request, 'index.html')
